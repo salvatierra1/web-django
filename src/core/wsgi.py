ee019fd3569ec3d11238a0a1498a6ef3c294fd88
salvatierra1 <victordanielsalvatierra6@gmail.com>
2024-08-01 20:36:49 -0300

    add js

--- src/core/wsgi.py
+++ src/core/wsgi.py
@@ -15,4 +15,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 #application = get_wsgi_application()
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+app = application
